a6807a6559ca06a9170cbd8ebac70c9536c0a9d1
Huateng Huang <huanghuateng@gmail.com>
2021-09-08 13:00:22 +1000

    uniform colnames

--- R/ABC_simulation_uniform.R
+++ R/ABC_simulation_uniform.R
@@ -43,6 +43,15 @@ ABC_simulation_uniform<-function(npod,conf,time.range,buffer=0,prefix='temp',Bay
       return(simulatehyperstat)
     }
   }
+  if(write.reference.file==T){
+    species<-as.character(unique(conf$species))
+    nspecies<-length(species)
+    data("reference.table")
+    colheads<-c("uid","nevent","nspecies")
+    colheads<-c(colheads,paste0("species",1:nspecies),colnames(reference.table)[grep("haptypes_Mean",colnames(reference.table)):dim(reference.table)[2]])
+    hsfile<-paste0(prefix,"_reference_table")
+    cat(paste(colheads,sep='',collapse = "\t"),"\n",sep='',file = hsfile,append = F)
+  }
 
   if(do.parallel==1){
     x<-sapply(1:npod,function(i)fx(i,conf,time.range,buffer,prefix,BayeSSCallocation,write.reference.file))
